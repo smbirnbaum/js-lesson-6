b7b05e512c7d4f1200aa922a7a5c15fdb6f77d05
korvmacka <shawnabirnbaum@email.arizona.edu>
2026-05-09 16:15:24 +0200

    Completed code added

--- scripts/l6.js
+++ scripts/l6.js
@@ -1,14 +1,69 @@
-function addBook() {
-  let title = prompt("Enter the book title:");
-  let author = prompt("Enter the author's name:");
-  let isReadAnswer = prompt("Have you read this book? yes/no");
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreElement = document.getElementById("score");
+const timeElement = document.getElementById("time");
+const endGameContainer = document.getElementById("end-game-container");
 
-  let book = {
-    title: title,
-    author: author,
-    isRead: isReadAnswer.toLowerCase() === "yes"
-  };
+const words = [
+  "cat",
+  "dog",
+  "pizza",
+  "banana",
+  "computer",
+  "javascript",
+  "school",
+  "flower",
+  "music",
+  "coffee"
+];
 
-  library.push(book);
-  alert("Book added!");
+let randomWord;
+let score = 0;
+let time = 10;
+
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
 }
+
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerText = randomWord;
+}
+
+function updateScore() {
+  score++;
+  scoreElement.innerText = score;
+}
+
+function updateTime() {
+  time--;
+  timeElement.innerText = time;
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    gameOver();
+  }
+}
+
+function gameOver() {
+  endGameContainer.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Play Again</button>
+  `;
+
+  endGameContainer.style.display = "flex";
+}
+
+text.addEventListener("input", function () {
+  if (text.value === randomWord) {
+    updateScore();
+    addWordToDOM();
+    time += 5;
+    text.value = "";
+  }
+});
+
+addWordToDOM();
+
+const timeInterval = setInterval(updateTime, 1000);
